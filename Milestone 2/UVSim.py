148bb7f485f359e73341efaebe9d9a73aca26ae2
Jackson Williams <jacksonwilliams.williams@gmail.com>
2024-09-11 17:50:05 -0600

    adds barebones UVSim.py

---
+++ Milestone 2/UVSim.py
@@ -0,0 +1,43 @@
+def read():
+    pass
+
+def write():
+    pass
+
+def load():
+    pass
+
+def store():
+    pass
+
+def add():
+    pass
+
+def subtract():
+    pass
+
+def divide():
+    pass
+
+def mulitply():
+    pass
+
+def branch():
+    pass
+
+def branch_neg():
+    pass
+
+def branch_zero():
+    pass
+
+def halt():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
