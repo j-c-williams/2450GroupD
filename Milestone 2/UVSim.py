02a685357318aa3d47ec6808e7466464b7cbf3f8
Kyler Cressall <kyler@cressall.net>
2024-09-14 18:14:09 -0600

    Added basic UVSim.py main method and test files

--- Milestone 2/UVSim.py
+++ Milestone 2/UVSim.py
@@ -34,9 +34,69 @@ def branch_zero():
 def halt():
     pass
 
+def read_txt_file():
+  global words
+  with open(file, 'r') as f:
+    for word in f.read().split():
+      words.append(word)
+  
+  if len(words) == 0:
+    raise ValueError("text file must have at least one word")
+
+file = "Test1.txt"
+accumulator = 0
+words = []
+pointer = 0
+
 
 def main():
-    pass
+  read_txt_file()
+
+  while(True):
+    global pointer
+    word = words[pointer]
+    word = word[:3] # grab only first 3 characters
+    
+    match word:
+      case "+10":
+        print("read")
+        read()
+      case "+11":
+        print("write")
+        write()
+      case "+20":
+        print("load")
+        load()
+      case "+21":
+        print("store")
+        store()
+      case "+30":
+        print("add")
+        add()
+      case "+31":
+        print("subtract")
+        subtract()
+      case "+32":
+        print("divide")
+        divide()
+      case "+33":
+        print("multiply")
+        mulitply()
+      case "+40":
+        print("branch")
+        branch()
+      case "+41":
+        print("branchneg")
+        branch_neg()
+      case "+42":
+        print("branchzero")
+        branch_zero()
+      case "+43":
+        print("halt")
+        break
+
+    pointer += 1
+
 
 
 if __name__ == "__main__":
