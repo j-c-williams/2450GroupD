adf2bb5b48ce4e65f7bc6b99b12f4c916e7ab6ab
Kyler Cressall <kyler@cressall.net>
2024-09-20 13:27:55 -0600

    The write unit tests were named 'read', fixed it

--- Milestone 2/test_write.py
+++ Milestone 2/test_write.py
@@ -1,18 +1,18 @@
 import unittest
 from UVSim import write, words
 
-class TestReadFunction(unittest.TestCase):
+class TestWriteFunction(unittest.TestCase):
 
     def setUp(self):
         global words
         words = [""] * 100
         words[5] = "test_word"
 
-    def test_read_valid_location(self):
+    def test_write_valid_location(self):
         result = write(5, words)
         self.assertEqual(result, "test_word")
 
-    def test_read_empty_location(self):
+    def test_write_empty_location(self):
         # Test reading from an empty location
         result = write(10, words)
         self.assertEqual(result, "")
